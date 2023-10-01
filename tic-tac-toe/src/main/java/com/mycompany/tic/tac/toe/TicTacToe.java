5735b8d4d5553c993c11ee2ff2eba4537dc70923
kl-j <96321586+kl-j@users.noreply.github.com>
2023-10-01 12:05:45 -0600

    Edit files

--- tic-tac-toe/src/main/java/com/mycompany/tic/tac/toe/TicTacToe.java
+++ tic-tac-toe/src/main/java/com/mycompany/tic/tac/toe/TicTacToe.java
@@ -5,12 +5,28 @@
 package com.mycompany.tic.tac.toe;
 
 /**
- *
+ *PLATAFORMAS ABIERTAS 1
+ * tic tac toe
  * @author jade
  */
-public class TicTacToe {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+
+public class TicTacToe {
+     public static void main(String[] args) {
+         
+         
+         char[][] gameBoard = new char[3][3];
+         System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(gameBoard[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+         //{{ },{|},{ }, {|}, { }};
+         
+         
+     }
 }
+    
